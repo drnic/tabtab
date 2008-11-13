b86ac6e3eff32856697f8ede5c3eaf57b0112eab
Dr Nic Williams <drnicwilliams@gmail.com>
2008-11-13 10:55:24 +1000

    fixed refereces to spec_helper to specs run

--- lib/ezy_auto_completions.rb
+++ lib/ezy_auto_completions.rb
@@ -3,4 +3,16 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 module EzyAutoCompletions
   VERSION = '0.0.1'
+  
+  class ExtractHelpOptions
+    def initialize(options_str)
+      @options_str = options_str
+    end
+    
+    # Returns list of long and short options for an application's --help display
+    # which was passed into initializer
+    def extract
+      []
+    end
+  end
 end
--- spec/extract_help_options_spec.rb
+++ spec/extract_help_options_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe EzyAutoCompletions::ExtractHelpOptions, "extract" do
   before(:each) do
