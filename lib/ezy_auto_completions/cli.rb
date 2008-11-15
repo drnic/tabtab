a5e96b49ac6b2f31106020e1e48586cdd056b7ec
Dr Nic Williams <drnicwilliams@gmail.com>
2008-11-15 13:13:39 +1030

    Clean up specs for expected 4 arguments to ezy_auto_completions

--- lib/ezy_auto_completions/cli.rb
+++ lib/ezy_auto_completions/cli.rb
@@ -4,16 +4,19 @@ module EzyAutoCompletions
   class CLI
     include EzyAutoCompletions::LocalConfig
 
+    attr_reader :app_type, :app_name, :previous, :current
+
     def self.execute(stdout, arguments=[])
       self.new.execute(stdout, arguments)
     end
     
     def execute(stdout, arguments=[])
+      usage unless @app_type = arguments.shift
+      @app_name, @current, @previous = arguments
       usage unless config # TODO not fatal - try --auto-completion-script option?
-      app, *args = arguments
-      options_flag = externals.find { |flag, app_list| app_list.include?(app) }
+      options_flag = externals.find { |flag, app_list| app_list.include?(app_name) }
       options_flag = options_flag.nil? ? '-h' : options_flag.first
-      stdout.puts external_options(app, options_flag).starts_with(args.first)
+      stdout.puts external_options(app_name, options_flag).starts_with(current)
     end
     
     def externals
--- spec/ezy_auto_completions_cli_spec.rb
+++ spec/ezy_auto_completions_cli_spec.rb
@@ -1,16 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'ezy_auto_completions/cli'
 
-describe EzyAutoCompletions::CLI, "execute for all options" do
+describe EzyAutoCompletions::CLI, "execute external for all options" do
   before(:each) do
     @cli = EzyAutoCompletions::CLI.new
     @options = mock do
-      expects(:starts_with).with(nil).returns(['--help', '--extra', '-h', '-x'])
+      expects(:starts_with).with('').returns(['--help', '--extra', '-h', '-x'])
     end
     @cli.expects(:external_options).with('test_app', '-h').returns(@options)
     @cli.expects(:config).returns({"external" => {"-h" => %w[test_app]}}).at_least(2)
     @stdout_io = StringIO.new
-    @cli.execute(@stdout_io, %w[test_app])
+    @cli.execute(@stdout_io, ['--external', 'test_app', '', 'test_app'])
     @stdout_io.rewind
     @stdout = @stdout_io.read
   end
