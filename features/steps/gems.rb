d0ff939ab3372a1e23d17c8b5ad9852a5a7e8de5
Dr Nic Williams <drnicwilliams@gmail.com>
2008-11-16 13:48:00 +1030

    installing a gem into user's gem cache for testing; includes a register call

--- features/steps/gems.rb
+++ features/steps/gems.rb
@@ -4,6 +4,8 @@ end
 
 Given /^a RubyGem '(.*)' with executable '(.*)' with autocompletions$/ do |gem, executable|
   gem_path = File.join(Gem.user_dir, "gems", gem)
-  %w[bin lib].each { |path| FileUtils.mkdir_p(File.join(gem_path, path)) }
-  FileUtils.cp(File.dirname(__FILE__) + "/../fixtures/bin/test_app", File.join(gem_path, 'bin', executable))
+  FileUtils.cp_r(File.dirname(__FILE__) + "/../fixtures/gems/#{gem}", gem_path)
+  Dir[File.join(gem_path, "/#{gem}/**/*.rb_")].each do |file|
+    FileUtils.mv(file, file.gsub(/rb_$/, 'rb'))
+  end
 end
