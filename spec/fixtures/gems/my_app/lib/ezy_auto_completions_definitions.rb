151a8798210d79179902dfa767db6c79373e93d6
Dr Nic Williams <drnicwilliams@gmail.com>
2008-11-16 17:40:07 +1030

    added more fancy stuff to demo test_app completions

--- spec/fixtures/gems/my_app/lib/ezy_auto_completions_definitions.rb
+++ spec/fixtures/gems/my_app/lib/ezy_auto_completions_definitions.rb
@@ -1,4 +1,16 @@
 EzyAutoCompletions::Definition.register('test_app') do |c|
   c.flags :help, :h
   c.flags :extra, :x
+  c.command :shell
+  c.command :do do
+    %w[run start end]
+  end
+  c.command :here do |here|
+    here.flag :funny, :f
+    here.command :we do |we|
+      we.default do
+        `git branch | sed -e "s/..//"`.split(/\n/)  
+      end
+    end
+  end
 end
