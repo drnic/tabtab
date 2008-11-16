2584b8d44c832ea914cec1849e18b6b982c7a27f
Dr Nic Williams <drnicwilliams@gmail.com>
2008-11-16 21:00:30 +1030

    completion definitions for rails and (ruby)gem

--- spec/fixtures/gems/my_app/lib/ezy_auto_completions_definitions.rb
+++ spec/fixtures/gems/my_app/lib/ezy_auto_completions_definitions.rb
@@ -36,3 +36,81 @@ EzyAutoCompletions::Definition.register('script/server') do |c|
     Dir['config/environments/*.rb'].map { |env| env.gsub(/^.*environments\//,'').gsub(/.rb$/,'') }
   end
 end
+
+EzyAutoCompletions::Definition.register('rails') do |c|
+  c.flags :freeze, :f
+  c.flags :version, :v
+  c.flags :help, :h
+  c.flags :pretend, :p
+  c.flags :force
+  c.flags :skip, :s
+  c.flags :quiet, :q
+  c.flags :backtrace, :t
+  c.flags :svn, :c
+  c.flags :git, :g
+  c.flags :database, :d do
+    "mysql/oracle/postgresql/sqlite2/sqlite3/frontbase".split('/')
+  end
+  c.flags :ruby, :r do
+    ENV['PATH'].split(":").inject([]) do |mem, path|
+      ruby = File.join(path, "ruby")
+      mem << ruby if File.exists?(ruby)
+      mem
+    end
+  end
+end
+
+EzyAutoCompletions::Definition.register('gem') do |c|
+  c.flags :version, :v
+  c.flags :h
+  
+  c.command :build do
+    Dir['**/*.gemspec']
+  end
+  c.command :check do |check|
+    check.flags :a, :alien
+    check.flags :t, :test
+  end
+  c.command :contents do
+    `gem list --local --no-versions --no-details`.split(/\n/)[3..-1]
+  end
+  c.command :dependency do
+    `gem list --local --no-versions --no-details`.split(/\n/)[3..-1]
+  end
+  c.command :install do |install|
+    Dir['**/*.gem']
+    # TODO this is very slow and may require caching somehow?!
+    install.default do
+      `gem list --remote --no-versions --no-details`.split(/\n/)[3..-1]
+    end
+  end
+  c.command :outdated do |outdated|
+    outdated.flags :local, :l
+    outdated.flags :remote, :r
+    outdated.flags :source
+    outdated.flags :platform do
+      Gem.platforms.map { |platform| platform.to_s }
+    end
+  end
+  c.command :specification do |spec|
+    spec.default do
+      `gem list --local --no-versions --no-details`.split(/\n/)[3..-1]
+    end
+    spec.flags :local, :l
+    spec.flags :remote, :r
+    spec.flags :source
+    spec.flags :platform do
+      Gem.platforms.map { |platform| platform.to_s }
+    end
+  end
+  c.command :uninstall do |uninstall|
+    uninstall.default do
+      `gem list --local --no-versions --no-details`.split(/\n/)[3..-1]
+    end
+  end
+  c.command :help do
+    ['commands'] + EzyAutoCompletions::Definition['gem'].contents.select do |definition|
+      definition.definition_type == :command
+    end.map { |definition| definition.name }
+  end
+end
