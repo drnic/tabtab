66e6ef335e4bc325c1c53bd00a7ab45499b7268c
Dr Nic Williams <drnicwilliams@gmail.com>
2008-11-17 21:14:04 +1030

    cleaned up internal fixture gem

--- spec/fixtures/gems/my_app/lib/tabtab_definitions.rb
+++ spec/fixtures/gems/my_app/lib/tabtab_definitions.rb
@@ -1,21 +1,5 @@
 TabTab::Definition.register('test_app') do |c|
   c.flags :help, :h, "help"
   c.flags :extra, :x
-  c.command :root do
-    puts "root"
-    %w[aaa bbb]
-  end
-  c.command :outer do |o|
-    o.default do
-      puts "outer"
-      %w[aaa bbb]
-    end
-    o.command :middle do |m|
-      m.command :inner do
-        puts "inner"
-        %w[aaa bbb]
-      end
-    end
-  end
 end
 
