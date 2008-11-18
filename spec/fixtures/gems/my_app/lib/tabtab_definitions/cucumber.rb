1a44b728dfc1e251d0af3c80021a0ff66b3cdd49
Dr Nic Williams <drnicwilliams@gmail.com>
2008-11-18 21:07:33 +1000

    added cucumber definitions

---
+++ spec/fixtures/gems/my_app/lib/tabtab_definitions/cucumber.rb
@@ -0,0 +1,19 @@
+TabTab::Definition.register('cucumber', :import => '--help') do |c|
+  c.flags(:profile, :p) do
+    next [] unless File.exists?('cucumber.yml')
+    require 'yaml'
+    YAML.load(File.read('cucumber.yml')).keys
+  end
+  c.flags(:format, :f) do
+    help = `cucumber -h`
+    languages = help.match(/Available formats:(.*)$/)[1]
+    languages.split(/,\s*/)
+  end
+  c.flags(:language, :a) do
+    help = `cucumber -h`
+    languages = help.match(/Available languages:(.*)$/)[1]
+    languages.split(/,\s*/)
+  end
+  c.flags(:color, :c)
+  c.flags(:"no-color", :c)
+end
