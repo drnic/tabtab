c1d86356c9c78199f30d795f858e3ee3c5d1cd89
Dr Nic Williams <drnicwilliams@gmail.com>
2008-11-22 23:40:06 +1000

    fixed manifest for fixture gem

--- features/steps/gems.rb
+++ features/steps/gems.rb
@@ -9,9 +9,10 @@ end
 
 Given /^a RubyGem '(.*)' with executable '(.*)' with autocompletions$/ do |gem_name, executable|
   @stdout = File.expand_path(File.join(@tmp_root, "geminstall.txt"))
+  @stderr = File.expand_path(File.join(@tmp_root, "geminstall.err"))
   FileUtils.chdir(File.join(File.dirname(__FILE__) + "/../../spec/fixtures/gems/#{gem_name}")) do
-    system "rake gemspec > #{@stdout} 2> #{@stdout}"
-    system "gem build #{gem_name}.gemspec > #{@stdout} 2> #{@stdout}"
-    system "gem install --install-dir #{gem_install_dir} #{gem_name}*.gem > #{@stdout} 2> #{@stdout}"
+    system "rake gemspec > #{@stdout} 2> #{@stderr}"
+    system "gem build #{gem_name}.gemspec > #{@stdout} 2> #{@stderr}"
+    system "gem install --install-dir #{gem_install_dir} #{gem_name}*.gem > #{@stdout} 2> #{@stderr}"
   end
 end
