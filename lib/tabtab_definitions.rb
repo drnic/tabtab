31013675ede9aaf979a087679edbc3b1ba6f3836
Dr Nic Williams <drnicwilliams@gmail.com>
2008-11-22 23:34:45 +1000

    splitting bundled definitions into tabtab_definitions and dev_... folders; will auto-find + install the former and ignore the latter

--- lib/tabtab_definitions.rb
+++
@@ -1,5 +0,0 @@
-# initially we're only preloading these to demonstrate the purpose
-# some of the remaining definitions are not finished or buggy in some way
-%w[cucumber github newgem rails].each do |app_name|
-  load File.dirname(__FILE__) + "/tabtab_definitions/#{app_name}.rb"
-end
