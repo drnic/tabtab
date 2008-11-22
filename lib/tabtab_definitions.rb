1e142f75824cb5d5c69854efb1a7f7c1469f3811
Dr Nic Williams <drnicwilliams@gmail.com>
2008-11-22 23:29:51 +1000

    restrict initial set of automatically install completions are some are buggy and its too slow loading many from one gem each time you tabtab

--- lib/tabtab_definitions.rb
+++ lib/tabtab_definitions.rb
@@ -1,3 +1,5 @@
-Dir[File.dirname(__FILE__) + "/tabtab_definitions/**/*.rb"].each do |definition|
-  load definition
+# initially we're only preloading these to demonstrate the purpose
+# some of the remaining definitions are not finished or buggy in some way
+%w[cucumber github newgem rails].each do |app_name|
+  load File.dirname(__FILE__) + "/tabtab_definitions/#{app_name}.rb"
 end
